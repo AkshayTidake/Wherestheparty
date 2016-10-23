ae43c628d1f23916e69901d6da3c79455ec9a5c9
Pranav Dixit <pranavdixit20@gmail.com>
2016-10-23 16:22:00 +0530

    small changes

--- mobile/src/main/java/adservices/wherestheparty/Login.java
+++ mobile/src/main/java/adservices/wherestheparty/Login.java
@@ -3,73 +3,100 @@ package adservices.wherestheparty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
+
 public class Login extends FragmentActivity implements View.OnClickListener {
-TextView tv;
-LoginButton loginButton;
+    TextView tv;
+    Profile p;
+    Button home;
+    LoginButton loginButton;
     CallbackManager callbackManager;
     private static final String TAG = "FacebookLogin";
-@Override
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    FacebookSdk.sdkInitialize(getApplicationContext());
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.login);
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+            LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    Log.d(TAG, "facebook:onSuccess:" + loginResult);
 
+                    Intent i = new Intent(Login.this, WelcomePage.class);
+                    startActivity(i);
 
+                }
 
-    callbackManager = CallbackManager.Factory.create();
-    loginButton = (LoginButton)findViewById(R.id.login_button);
-    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            Intent i = new Intent(Login.this,WelcomePage.class);
-            startActivity(i);
+                @Override
+                public void onCancel() {
+                    Log.d(TAG, "facebook:onCancel");
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    Log.d(TAG, "facebook:onError", error);
+                }
+            });
+
+            tv = (TextView) findViewById(R.id.hiw);
+            home = (Button)findViewById(R.id.home);
+            tv.setOnClickListener(this);
+            home.setOnClickListener(this);
         }
 
         @Override
-        public void onCancel() {
-
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
+
 
         @Override
-        public void onError(FacebookException error) {
-
-        }
-    });
-
-    tv = (TextView)findViewById(R.id.hiw);
-    tv.setOnClickListener(this);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.hiw:
-                Intent i = new Intent(this,How_Its_Work.class);
-                startActivity(i);
-                break;
-            case R.id.login_button:
-                LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
-                break;
+        public void onClick (View v){
+            switch (v.getId()) {
+                case R.id.hiw:
+                    Intent i = new Intent(this, How_Its_Work.class);
+                    startActivity(i);
+                    break;
+                case R.id.login_button:
+                    LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"));
+                    break;
+                case R.id.home:
+                    Profile.getCurrentProfile();
+                    p = Profile.getCurrentProfile();
+                    if (p != null) {
+                        // user has logged in
+                        Intent in = new Intent(Login.this, WelcomePage.class);
+                        startActivity(in);
+                        finish();
+                    } else {
+                        // user has not logged in
+                        Toast.makeText(this,"Please Login your account",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+            }
         }
     }
-}
+
+
+
--- mobile/src/main/java/adservices/wherestheparty/MainActivity.java
+++ mobile/src/main/java/adservices/wherestheparty/MainActivity.java
@@ -1,29 +1,66 @@
 package adservices.wherestheparty;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tv;
+    Profile p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
         tv = (TextView)findViewById(R.id.gs);
         tv.setOnClickListener(this);
+
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "adservices.wherestheparty", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:",Base64.encodeToString(md.digest(), Base64.DEFAULT));
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.gs:
-                Intent i = new Intent(this,Login.class);
-                startActivity(i);
+                Profile.getCurrentProfile();
+                p = Profile.getCurrentProfile();
+                if (p != null) {
+                    // user has logged in
+                    Intent i = new Intent(MainActivity.this, WelcomePage.class);
+                    startActivity(i);
+                } else {
+                    // user has not logged in
+                    Intent i = new Intent(MainActivity.this, Login.class);
+                    startActivity(i);
+                }
                 break;
         }
     }
